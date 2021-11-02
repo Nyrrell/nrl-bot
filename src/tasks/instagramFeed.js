717c5b3d18f1d10170db2a7bf2a4d54092a352ff
Nyrrell <morel.bapt@gmail.com>
2021-11-02 17:33:40 +0100

    fix instagram photo url expire

--- src/tasks/instagramFeed.js
+++ src/tasks/instagramFeed.js
@@ -9,35 +9,33 @@ import { channels, guildId } from "../config.js";
 
 const baseURL = 'https://www.instagram.com/cirkajin/feed/?__a=1'
 
-const userName = (html) => html['graphql']['user']['full_name']
-const totalPhotos = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['count']
-const userPhoto = (html) => html['graphql']['user']['profile_pic_url']
-const lastPublication = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode']
-const lastPhotoUrl = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['display_url']
-const lastThumbUrl = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['thumbnail_src']
-const timestamp = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['taken_at_timestamp']
-const descriptionPhoto = (html) => html['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['edge_media_to_caption']['edges'][0]['node']['text']
-
 cron.schedule('* * * * *', async () => {
     try {
       const channel = await client.guilds.cache.get(guildId)?.channels.cache.get(channels['social'])
 
-      const html = await axios.get(baseURL).then(res => res.data)
-      if (await instagram.get('lastPublication') !== lastPublication(html)) {
-        await instagram.set('lastPublication', lastPublication(html))
+      const data = await axios.get(baseURL).then(res => res.data['graphql']?.['user'])
+      const lastPublication = data['edge_owner_to_timeline_media']['edges'][0]['node']
+      const userName = data['full_name']
+
+      const lastPhotoUrl = lastPublication['display_url']
+      const lastPublicationCode = lastPublication['shortcode']
+      const descriptionPhoto = lastPublication['edge_media_to_caption']['edges'][0]['node']['text']
+
+      if (await instagram.get('lastPublication') !== lastPublicationCode) {
+        await instagram.set('lastPublication', lastPublicationCode)
         await channel?.send({
           embeds: [
             new MessageEmbed()
               .setColor('#DD2A7B')
-              .setAuthor('Instagram', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/langfr-1024px-Instagram_logo_2016.svg.png')
-              .setTitle(`Nouvelle publication de ${userName(html)}`)
-              .setURL(`https://www.instagram.com/p/${lastPublication(html)}/`)
-              .setDescription(descriptionPhoto(html))
-              .setThumbnail(`attachment://${lastPublication(html)}.webp`)
+              .setAuthor('Instagram', 'https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png')
+              .setTitle(`Nouvelle publication de ${userName}`)
+              .setURL(`https://www.instagram.com/p/${lastPublicationCode}/`)
+              .setDescription(descriptionPhoto)
+              .setThumbnail(`attachment://${lastPublicationCode}.webp`)
           ],
           files: [{
-            attachment: lastPhotoUrl(html),
-            name: `${lastPublication(html)}.webp`
+            attachment: lastPhotoUrl,
+            name: `${lastPublicationCode}.webp`
           }]
         })
       }
