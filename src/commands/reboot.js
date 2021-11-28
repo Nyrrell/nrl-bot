ac661c32e66d858348e2b15f2e753d88a6a19e49
nyrrell <morel.bapt@gmail.com>
2021-11-28 17:50:28 +0100

    reboot interaction

---
+++ src/commands/reboot.js
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const command =  {
+    permissions: {
+        id: '343169638336561154',
+        type: 'USER',
+        permission: true
+    },
+    data: new SlashCommandBuilder()
+        .setName('reboot')
+        .setDescription('Redémarre le bot [Nyrrell]'),
+    async execute(interaction) {
+        await interaction.reply({ content: 'Reboot incoming', ephemeral: true });
+        process.exit();
+    },
+};
