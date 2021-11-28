c2511b7b536f9940adb2b7cb501e5cc40d2260b6
nyrrell <morel.bapt@gmail.com>
2021-11-28 17:29:04 +0100

    fix guildMemberAdd.js invalid-user (plz)

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -7,13 +7,11 @@ export const event = {
   description: "When a member join the guild",
   async execute(member) {
     const { user, guild } = member
-
-    if (member.user.bot || member.guild.id !== guildId) return;
+    if (user.bot || guild.id !== guildId) return;
 
     try {
       member.guild.channels.cache.get(channels['taverne'])?.send({
       content: `<@${user.id}> vient de débarquer sur le serveur **${guild.name}**, bienvenue ${user.username} 👋`,
-        allowedMentions: {users: []}
       })
     } catch (error) {
       logger.error(error);
