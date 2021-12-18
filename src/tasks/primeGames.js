7899019ccad463fab286696ec105d2ff24e79a51
nyrrell <morel.bapt@gmail.com>
2021-12-18 11:15:03 +0100

    refacto function primeGames

--- src/tasks/primeGames.js
+++ src/tasks/primeGames.js
@@ -1,18 +1,24 @@
 import axios from "axios";
-
-import { channels, color, guildId } from "../config.js";
-import { primeGamingKeyv } from "../services/keyv.js";
-import logger from "../services/logger.js";
 import { MessageEmbed } from "discord.js";
+
 import { client } from '../app.js';
+import logger from "../services/logger.js";
+import { primeGamingKeyv } from "../services/keyv.js";
+import { channels, color, guildId } from "../config.js";
+
+const primeGamingApi = axios.create({
+  baseURL: 'https://gql.twitch.tv/gql',
+  headers: {
+    "Accept-Language": "fr-FR",
+    "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"
+  }
+})
 
 export const primeGames = async () => {
   try {
     const channel = await client.guilds.cache.get(guildId)?.channels.cache.get(channels['tips'])
-    const storeURL = 'https://gaming.amazon.com/loot/'
-    const currentOffer = []
 
-    const primeOfferContent = await axios.post("https://gql.twitch.tv/gql", {
+    const primeOfferContent = [] = await primeGamingApi.post("", {
       "operationName": "Prime_PrimeOfferList_PrimeOffers_Eligibility",
       "variables": {},
       "extensions": {
@@ -21,47 +27,36 @@ export const primeGames = async () => {
           "sha256Hash": "8002dbaac2b59f770515e7aa901484a78d9150e54283c3b21f2229f83eca48cf"
         }
       }
-    }, {
-      headers: {
-        "Accept-Language": "fr-FR",
-        "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"
-      }
     }).then(({ data }) => data.data?.['primeOffersWithEligibility'])
+    const offersId = primeOfferContent.map(offer => offer['id']);
+
+    const storedOffers = await primeGamingKeyv.get('latestOffers');
 
     for (const offer of primeOfferContent) {
-      currentOffer.push(offer['id'])
+      if (storedOffers?.includes(offer['id'])) continue;
 
-      if (!await primeGamingKeyv.has(offer['id'])) {
-        await primeGamingKeyv.set(offer['id'], true)
-
-        let description = ""
-        if (offer['deliveryMethod'] === "DIRECT_ENTITLEMENT" || offer['tags'].includes('FGWP')) {
-          const config = offer['description'].indexOf('MINIMALE')
-          config !== -1 ? description = offer['description'].slice(0, config) : description = offer['description']
-        }
-
-        await channel?.send({
-          embeds:
-            [
-              new MessageEmbed()
-                .setColor(color.blue)
-                .setAuthor('Prime Gaming', 'https://m.media-amazon.com/images/G/01/sm/shared/crown_animation_loop.gif')
-                .setTitle(offer['title'])
-                .setURL(offer['content']['externalURL'] || storeURL)
-                .setDescription(description)
-                .setImage(offer['imageURL'])
-                .setFooter(offer['content']['categories'][0])
-            ]
-        })
+      let description;
+      if (offer['deliveryMethod'] === "DIRECT_ENTITLEMENT" || offer['tags'].includes('FGWP')) {
+        const config = offer['description'].indexOf('MINIMALE');
+        config !== -1 ? description = offer['description'].slice(0, config) : description = offer['description'];
       }
-    }
 
-    const storedOffer = await primeGamingKeyv.iterator()
-    for await(const offer of storedOffer) {
-      if (!currentOffer.includes(offer[0])) await primeGamingKeyv.delete(offer[0])
-    }
+      await channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor(color.blue)
+            .setAuthor('Prime Gaming', 'https://m.media-amazon.com/images/G/01/sm/shared/crown_animation_loop.gif')
+            .setTitle(offer['title'])
+            .setURL(offer['content']['externalURL'] || 'https://gaming.amazon.com/loot/')
+            .setDescription(description || "")
+            .setImage(offer['imageURL'])
+            .setFooter(offer['content']['categories'][0])
+        ]
+      });
 
+      await primeGamingKeyv.set("latestOffers", offersId);
+    }
   } catch (e) {
-    logger.error(e)
+    logger.error(e);
   }
 }
