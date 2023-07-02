9d9dea32d1df7137fc20d28e43eb5a531a743614
nyrrell <morel.bapt@gmail.com>
2023-07-02 09:12:06 +0200

    daily task change hour

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -26,7 +26,7 @@ cron.schedule('0 2 * * *', async () => {
 }, { timezone: timezone });
 
 // Post daily subs
-cron.schedule('0 9 * * *', async () => {
+cron.schedule('0 8 * * *', async () => {
   await postDailySubs();
 }, { timezone: timezone });
 
